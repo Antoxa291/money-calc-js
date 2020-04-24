19d9597784c13f8c8e7496ace3ca2fe492d2e3aa
Antoxa <antoxa2918@gmail.com>
2020-04-24 21:06:00 +0300

    add functionals

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 "use strict";
+let money, time;
 
-let money = +prompt("Ваш бюджет на месяц?", ""),
+function start() {
+  money = +prompt("Ваш бюджет на месяц?", "");
+
+  while (isNaN(money) || money == "" || money == null) {
+    money = +prompt("Ваш бюджет на месяц?", "");
+  }
+
   time = prompt("Введите дату в формате YYYY-MM-DD", "");
+}
+start();
 
 let appData = {
   budget: money,
@@ -9,76 +18,51 @@ let appData = {
   optionalExpenses: {},
   income: [],
   timeData: time,
-  savings: false,
+  savings: true,
 };
-// .................FOR..................
-for (let i = 0; i < 2; i++) {
-  let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    b = +prompt("Во сколько обойдется?", "");
 
-  if (
-    typeof a === "string" &&
-    typeof a != null &&
-    typeof b != null &&
-    a != "" &&
-    b != "" &&
-    a.length < 50
-  ) {
-    appData.expenses[a] = b;
-  } else {
-    alert("Введено не допустимое значение, попробуйте еще раз.");
-    i--;
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+      b = +prompt("Во сколько обойдется?", "");
+
+    if (
+      typeof a === "string" &&
+      typeof a != null &&
+      typeof b != null &&
+      a != "" &&
+      b != "" &&
+      a.length < 50
+    ) {
+      appData.expenses[a] = b;
+    } else {
+      alert("Введено не допустимое значение, попробуйте еще раз.");
+      i--;
+    }
   }
 }
-// ..........WHILE..........
-// let i = 0;
-// while (i < 2) {
-//   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//   b = +prompt("Во сколько обойдется?", "");
-//   if (typeof a === "string" &&
-//     typeof a != null &&
-//     typeof b != null &&
-//     a != "" &&
-//     b != "" &&
-//     a.length < 50) {
-//     appData.expenses[a] = b;
-//   } else {
-//     alert("Введено не допустимое значение, попробуйте еще раз.");
-//     i--;
-//   }
-//     i++;
-//   }
+chooseExpenses();
 
-// ...........DO...WHILE....................
-// let i = 1;
-// do {
-//   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-//     b = +prompt("Во сколько обойдется?", "");
-//   if (
-//     typeof a === "string" &&
-//     typeof a != null &&
-//     typeof b != null &&
-//     a != "" &&
-//     b != "" &&
-//     a.length < 50
-//   ) {
-//     appData.expenses[a] = b;
-//   } else {
-//     alert("Введено не допустимое значение, попробуйте еще раз.");
-//     i--;
-//   }
-//   i++;
-// } while (i < 2);
-
-appData.moneyPerDay = appData.budget / 30;
+appData.moneyPerDay = (appData.budget / 30).toFixed(2);
 alert("Ежедневный бюджет" + appData.moneyPerDay);
 
-if (appData.moneyPerDay < 10000 || appData.moneyPerDay == 10000) {
+if (appData.moneyPerDay < 200 || appData.moneyPerDay == 200) {
   console.log("Уровень достатка ниже среднего.");
-} else if (appData.moneyPerDay > 10000 && appData.moneyPerDay < 20000) {
+} else if (appData.moneyPerDay > 200 && appData.moneyPerDay < 500) {
   console.log("Средний уровень достатка.");
-} else if (appData.moneyPerDay > 20000) {
+} else if (appData.moneyPerDay > 499) {
   console.log("Высокий уровень достатка.");
 } else {
   console.log("Проверка не пройдена!");
 }
+
+function checkSavings() {
+  if (appData.savings == true) {
+    let save = +prompt("Какова сумма накоплений?", ""),
+      percent = +prompt("Каков процент в год?", "");
+
+    appData.monthIncome = (save / 100 / 12) * percent;
+    alert("Ваш доход с накоплений в месяц =" + appData.monthIncome.toFixed(2));
+  }
+}
+checkSavings();
